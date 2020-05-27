876d83eb5c671beb2b2016ccce812cb53f12ec11
AbisurDiazR <abisurdiazramirez@gmail.com>
2020-05-26 21:37:57 -0500

    Añadir bootstrap

--- ejemplologin/src/app/app.module.ts
+++ ejemplologin/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './autenticacion/login/login.component';
+import { RegisterComponent } from './autenticacion/register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
